d401cfc34690592a3868b88ff56c54c785df9301
fart-gllitch <daquonhardman22@gmail.com>
2025-04-08 22:51:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
         "https://helios-browser.rf.gd",
         "https://helios-browser.w3spaces.com/",
         "https://helios-browser.pages.dev/",
-        "https://helios-browser.ct.ws"
+        "https://helios-browser.ct.ws",
+        "https://arcane.wtf"
     ];
 
     function normalizeUrl(url) {
